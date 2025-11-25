05a5d3f2baf148464a649aa773c5a5f570782c43
kristinelalala <kristineroselin@gmail.com>
2025-11-25 17:38:09 +0800

    Update AIChatModal.tsx

--- AIChatModal.tsx
+++ AIChatModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { X, Send, Bot, User, Sparkles } from 'lucide-react';
-import { ChatMessage } from '../types';
+import { ChatMessage } from './types';
 import { sendMessageToGemini } from ''./geminiService'';
 import ReactMarkdown from 'react-markdown';
 
